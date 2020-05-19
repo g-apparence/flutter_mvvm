2f03f79f27b5379643f14577ebc86524eadfebab
Gautier <gautier@apparence.io>
2020-05-19 09:53:16 +0200

    next on exports

--- lib/mvvm_builder.dart
+++ lib/mvvm_builder.dart
@@ -9,8 +9,8 @@ import 'presenter_builder.dart';
 export 'component_builder.dart';
 export 'mvvm_model.dart';
 export 'component_animated_builder.dart';
-export 'component_builder.dart';
 export 'presenter_builder.dart';
+export 'mvvm_context.dart';
 
 class MvvmBuilder {
   static const MethodChannel _channel = const MethodChannel('mvvm_builder');
