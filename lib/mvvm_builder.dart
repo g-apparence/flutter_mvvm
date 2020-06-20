d6b363d3c9785bed7f8fb0ff03b98338eb24e872
miyoyo <miyoyo@whiterhats.be>
2020-06-20 11:27:34 +0200

    Sort exports, because apparently that's a thing

--- lib/mvvm_builder.dart
+++ lib/mvvm_builder.dart
@@ -1,6 +1,5 @@
-export 'component_builder.dart';
-export 'mvvm_model.dart';
 export 'component_animated_builder.dart';
-export 'presenter_builder.dart';
+export 'component_builder.dart';
 export 'mvvm_context.dart';
-
+export 'mvvm_model.dart';
+export 'presenter_builder.dart';
