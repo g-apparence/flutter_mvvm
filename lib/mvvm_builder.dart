2b637ee27932364a8054e22d534870a6f37d4964
miyoyo <miyoyo@whiterhats.be>
2020-06-20 11:22:12 +0200

    All you need are exports

--- lib/mvvm_builder.dart
+++ lib/mvvm_builder.dart
@@ -1,25 +1,6 @@
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'component_builder.dart';
-import 'mvvm_model.dart';
-import 'presenter_builder.dart';
-
 export 'component_builder.dart';
 export 'mvvm_model.dart';
 export 'component_animated_builder.dart';
 export 'presenter_builder.dart';
 export 'mvvm_context.dart';
-
-class MvvmBuilder {
-  static const MethodChannel _channel = const MethodChannel('mvvm_builder');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-
-}
 
